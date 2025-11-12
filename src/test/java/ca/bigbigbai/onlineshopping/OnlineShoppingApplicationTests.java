4541e90e29439cad240f18ba873b2ac9c5b76af2
BigBigBai <baiyingjian@outlook.com>
2025-11-12 16:39:42 -0500

    setup spring-boot

---
+++ src/test/java/ca/bigbigbai/onlineshopping/OnlineShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bigbigbai.onlineshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
